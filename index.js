791b2da38c019a4e329dcd3a856461010e304c11
AugustoPaiva <augustopaiva4@gmail.com>
2019-06-16 14:15:20 -0300

    conexão com o chart.js

--- index.js
+++ index.js
@@ -1,42 +1,84 @@
 var map, heatmap;
 var trafficViolations = [];
 
-// window.onload =  function(){
-//     getTrafficViolations();
-//     console.log(trafficViolations);
-    
-// }
-function getTrafficViolations(){
-    var http =  new XMLHttpRequest();
-    http.onreadystatechange =  function(){
-        if(http.readyState == 4 && http.status == 200){
-            trafficViolations = (JSON.parse(http.response)).TrafficViolations;
-        }
+window.onload = function() {
+  loadcanva();
+};
+function getTrafficViolations() {
+  var http = new XMLHttpRequest();
+  http.onreadystatechange = function() {
+    if (http.readyState == 4 && http.status == 200) {
+      trafficViolations = JSON.parse(http.response).TrafficViolations;
     }
-    http.open("GET", "trafficViolations.json", false);
-    http.send();
+  };
+  http.open("GET", "trafficViolations.json", false);
+  http.send();
 }
 
 async function initMap() {
-    await getTrafficViolations();
-    map = new google.maps.Map(document.getElementById('map'), {
-        zoom: 13,
-        center: {lat: 37.775, lng: -122.434},
-        mapTypeId: 'satellite'
-    });
+  await getTrafficViolations();
+  map = new google.maps.Map(document.getElementById("map"), {
+    zoom: 13,
+    center: { lat: 37.775, lng: -122.434 },
+    mapTypeId: "satellite"
+  });
 
-    heatmap = new google.maps.visualization.HeatmapLayer({
-        data: getPoints(),
-        map: map
-    });
+  heatmap = new google.maps.visualization.HeatmapLayer({
+    data: getPoints(),
+    map: map
+  });
 }
 
-function getPoints(){
-    var retorno = [];
-    trafficViolations.forEach(trafficViolation => {
-        retorno.push(new google.maps.LatLng(trafficViolation.cord[0],trafficViolation.cord[1]))
-    });
-    return retorno;
+function getPoints() {
+  var retorno = [];
+  trafficViolations.forEach(trafficViolation => {
+    retorno.push(
+      new google.maps.LatLng(trafficViolation.cord[0], trafficViolation.cord[1])
+    );
+  });
+  return retorno;
 }
 
-
+function loadcanva() {
+  var ctx = document.getElementById("myChart").getContext("2d");
+  var myChart = new Chart(ctx, {
+    type: "bar",
+    data: {
+      labels: ["Red", "Blue", "Yellow", "Green", "Purple", "Orange"],
+      datasets: [
+        {
+          label: "# of Votes",
+          data: [12, 19, 3, 5, 2, 3],
+          backgroundColor: [
+            "rgba(255, 99, 132, 0.2)",
+            "rgba(54, 162, 235, 0.2)",
+            "rgba(255, 206, 86, 0.2)",
+            "rgba(75, 192, 192, 0.2)",
+            "rgba(153, 102, 255, 0.2)",
+            "rgba(255, 159, 64, 0.2)"
+          ],
+          borderColor: [
+            "rgba(255, 99, 132, 1)",
+            "rgba(54, 162, 235, 1)",
+            "rgba(255, 206, 86, 1)",
+            "rgba(75, 192, 192, 1)",
+            "rgba(153, 102, 255, 1)",
+            "rgba(255, 159, 64, 1)"
+          ],
+          borderWidth: 1
+        }
+      ]
+    },
+    options: {
+      scales: {
+        yAxes: [
+          {
+            ticks: {
+              beginAtZero: true
+            }
+          }
+        ]
+      }
+    }
+  });
+}
