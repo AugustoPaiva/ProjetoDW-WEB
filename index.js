00cfeb786a46b00f4c22891fbf21639739f30936
AugustoPaiva <augustopaiva4@gmail.com>
2019-07-01 21:02:55 -0300

    plotando todos os graficos

--- index.js
+++ index.js
@@ -1,13 +1,31 @@
 var map, heatmap;
 var trafficViolations = [];
+window.bairros = [];
 
-window.onload = function () {
-    loadcanva();
+window.onload = async function () {
+    await loadcanva();
+    localStorage.setItem('bairros', []);
+    localStorage.setItem('ano', '');
 };
 
+document.getElementById('add').onclick = function () {
+    //click me function!
+    window.bairros.push(document.getElementById('bairro').value)
+    document.getElementById('bairro').value = '';
+    console.log(window.bairros);
+}
 
+document.getElementById('form').onsubmit = async function () {
+    console.log('oi');
+    localStorage.setItem('bairros', window.bairros);
+    localStorage.setItem('ano', document.getElementById('ano').value);
+    console.log(window.bairros);
+    console.log(window.ano);
+    //loadcanva();
+}
 
 async function initMap() {
+    console.log(localStorage.getItem('bairros').split(','));
     await getTrafficViolations();
     map = new google.maps.Map(document.getElementById('map'), {
         zoom: 11,
@@ -22,9 +40,15 @@ async function initMap() {
 
 async function getTrafficViolations() {
     try {
-        const pontos = await axios.get('https://projeto-dw.herokuapp.com/api/infracoes', {});
+        const bairros = localStorage.getItem('bairros').split(',');
+        const ano = localStorage.getItem('ano');
+        //const pontos = await axios.post('https://projeto-dw.herokuapp.com/api/infracoes', { 'ano': window.ano, 'bairro': window.bairros });
+        const pontos = await axios.post('http://localhost:3001/api/infracoes', { 'ano': ano, 'bairro': bairros });
+        //const pontos = await axios.post('https://projeto-dw.herokuapp.com/api/infracoes', { 'ano': window.ano, 'bairro': window.bairros });
+
+        //const pontos = await axios.get('https://projeto-dw.herokuapp.com/api/infracoes', {});
         trafficViolations = pontos.data;
-        console.log(trafficViolations);
+
 
     } catch (e) {
         console.log(e);
@@ -35,7 +59,6 @@ async function getTrafficViolations() {
 function getPoints() {
     var retorno = [];
     trafficViolations.forEach(trafficViolation => {
-        console.log(trafficViolation)
         for (const x of Array(trafficViolation.ocorrencias).keys()) {
             retorno.push(
                 new google.maps.LatLng(trafficViolation.latitude, trafficViolation.longitude)
@@ -49,28 +72,36 @@ function getPoints() {
 function loadcanva() {
     valorArrecadadoChart();
     qtdinfraChart();
-    qqq();
+    infraSemestre();
     tpInfraRecorrenteChart();
     picoInfraChart();
     pontosCarteiraChart();
 }
-function valorArrecadadoChart() {
+
+//FEITO
+async function valorArrecadadoChart() {
+    let labels = [];
+    let data = [];
+    const bairros = localStorage.getItem('bairros').split(',');
+    const ano = localStorage.getItem('ano');
+    const valor = await axios.post('http://localhost:3001/api/valorMulta', { 'ano': ano, 'bairro': bairros });
+
+    await valor.data[0].forEach(valor => { labels.push(valor.nome); data.push(parseInt(valor.total)) });
     var valorArrecadado = document.getElementById("valorArrecadado").getContext("2d");
     var myChart = new Chart(valorArrecadado, {
         type: "pie",
         data: {
-            labels: ["2015", "2016", "2017", "2018", "2019", "Orange"],
+            labels: labels,
             datasets: [
                 {
-                    label: "# of Votes",
-                    data: [1000, 2505, 6596, 3262, 3362, 3363],
+                    label: "Bairros",
+                    data: data,
                     backgroundColor: [
                         "rgba(255, 99, 132, 0.2)",
                         "rgba(54, 162, 235, 0.2)",
                         "rgba(255, 206, 86, 0.2)",
                         "rgba(75, 192, 192, 0.2)",
                         "rgba(153, 102, 255, 0.2)",
-                        "rgba(255, 159, 64, 0.2)"
                     ],
                     borderColor: [
                         "rgba(255, 99, 132, 1)",
@@ -78,7 +109,6 @@ function valorArrecadadoChart() {
                         "rgba(255, 206, 86, 1)",
                         "rgba(75, 192, 192, 1)",
                         "rgba(153, 102, 255, 1)",
-                        "rgba(255, 159, 64, 1)"
                     ],
                     borderWidth: 1
                 }
@@ -86,23 +116,31 @@ function valorArrecadadoChart() {
         }
     });
 }
-function qtdinfraChart() {
+async function qtdinfraChart() {
+    //quantidadeInfracoes
+    let labels = [];
+    let data = [];
+    const bairros = localStorage.getItem('bairros').split(',');
+    const ano = localStorage.getItem('ano');
+    const qtd = await axios.post('http://localhost:3001/api/quantidadeInfracoes', { 'ano': ano, 'bairro': bairros });
+
+    await qtd.data.forEach(valor => { labels.push(valor.nome); data.push(parseInt(valor.quantidade)) });
+
     var qtdinfra = document.getElementById("qtdinfra").getContext("2d");
     var myChart = new Chart(qtdinfra, {
-        type: "line",
+        type: "bar",
         data: {
-            labels: ["2015", "2016", "2017", "2018", "2019", "Orange"],
+            labels: labels,
             datasets: [
                 {
-                    label: "# of Votes",
-                    data: [1000, 2505, 6596, 3262, 3362, 3363],
+                    label: "Quantidade de infração",
+                    data: data,
                     backgroundColor: [
                         "rgba(255, 99, 132, 0.2)",
                         "rgba(54, 162, 235, 0.2)",
                         "rgba(255, 206, 86, 0.2)",
                         "rgba(75, 192, 192, 0.2)",
                         "rgba(153, 102, 255, 0.2)",
-                        "rgba(255, 159, 64, 0.2)"
                     ],
                     borderColor: [
                         "rgba(255, 99, 132, 1)",
@@ -110,7 +148,6 @@ function qtdinfraChart() {
                         "rgba(255, 206, 86, 1)",
                         "rgba(75, 192, 192, 1)",
                         "rgba(153, 102, 255, 1)",
-                        "rgba(255, 159, 64, 1)"
                     ],
                     borderWidth: 1
                 }
@@ -118,16 +155,25 @@ function qtdinfraChart() {
         }
     });
 }
-function qqq() {
-    var qqq = document.getElementById("qqq").getContext("2d");
-    var myChart = new Chart(qqq, {
-        type: "bar",
+async function infraSemestre() {
+    let labels = [];
+    let data = [];
+    const bairros = localStorage.getItem('bairros').split(',');
+    const ano = localStorage.getItem('ano');
+    const retorno = await axios.post('http://localhost:3001/api/infraSemestre', { 'ano': ano, 'bairro': bairros });
+
+    console.log(retorno.data);
+    await retorno.data.forEach(valor => { labels.push(`${valor.ano_texto}/${valor.semestre_texto[valor.semestre_texto.length - 1]}`); data.push(parseInt(valor.quantidade)) });
+
+    var infraSemestre = document.getElementById("infraSemestre").getContext("2d");
+    var myChart = new Chart(infraSemestre, {
+        type: "line",
         data: {
-            labels: ["2015", "2016", "2017", "2018", "2019", "Orange"],
+            labels: labels,
             datasets: [
                 {
                     label: "# of Votes",
-                    data: [1000, 2505, 6596, 3262, 3362, 3363],
+                    data: data,
                     backgroundColor: [
                         "rgba(255, 99, 132, 0.2)",
                         "rgba(54, 162, 235, 0.2)",
@@ -151,33 +197,28 @@ function qqq() {
     });
 }
 async function tpInfraRecorrenteChart() {
-    // var json = {};
-    // var http = await new XMLHttpRequest();
-    // http.onreadystatechange = await function () {
-    //     if (http.readyState == 4 && http.status == 200) {
-    //         json = JSON.parse(http.response);
-    //     }
-    // };
-    // console.log(json);
-    // http.open("GET", "http://localhost:3001/api/topInfracao", false);
-    // http.send();
+    let labels = [];
+    let data = [];
+    const bairros = localStorage.getItem('bairros').split(',');
+    const ano = localStorage.getItem('ano');
+    const infracao = await axios.post('http://localhost:3001/api/topInfracao', { 'ano': ano, 'bairro': bairros });
+    await infracao.data.forEach(valor => { labels.push((valor.infracao_id + '-' + valor.descricao)); data.push(parseInt(valor.quantidade)) });
 
     var tpInfraRecorrente = document.getElementById("tpInfraRecorrente").getContext("2d");
     var myChart = new Chart(tpInfraRecorrente, {
         type: "polarArea",
         data: {
-            labels: ["2015", "2016", "2017", "2018", "2019", "Orange"],
+            labels: labels,
             datasets: [
                 {
                     label: "# of Votes",
-                    data: [1000, 2505, 6596, 3262, 3362, 3363],
+                    data: data,
                     backgroundColor: [
                         "rgba(255, 99, 132, 0.2)",
                         "rgba(54, 162, 235, 0.2)",
                         "rgba(255, 206, 86, 0.2)",
                         "rgba(75, 192, 192, 0.2)",
                         "rgba(153, 102, 255, 0.2)",
-                        "rgba(255, 159, 64, 0.2)"
                     ],
                     borderColor: [
                         "rgba(255, 99, 132, 1)",
@@ -185,31 +226,43 @@ async function tpInfraRecorrenteChart() {
                         "rgba(255, 206, 86, 1)",
                         "rgba(75, 192, 192, 1)",
                         "rgba(153, 102, 255, 1)",
-                        "rgba(255, 159, 64, 1)"
                     ],
+
                     borderWidth: 1
                 }
             ]
+        },
+        options: {
+            legend: {
+                display: false
+            }
         }
     });
 }
-function picoInfraChart() {
+async function picoInfraChart() {
+    //horaPico
+    let labels = [];
+    let data = [];
+    const bairros = localStorage.getItem('bairros').split(',');
+    const ano = localStorage.getItem('ano');
+    const horapico = await axios.post('http://localhost:3001/api/horaPico', { 'ano': ano, 'bairro': bairros });
+    await horapico.data.forEach(valor => { labels.push(valor.hora + 'hr'); data.push(parseInt(valor.quantidade)) });
+
     var picoInfra = document.getElementById("picoInfra").getContext("2d");
     var myChart = new Chart(picoInfra, {
-        type: "radar",
+        type: "bar",
         data: {
-            labels: ["2015", "2016", "2017", "2018", "2019", "Orange"],
+            labels: labels,
             datasets: [
                 {
-                    label: "# of Votes",
-                    data: [1000, 2505, 6596, 3262, 3362, 3363],
+                    label: "Quantidade de infração",
+                    data: data,
                     backgroundColor: [
                         "rgba(255, 99, 132, 0.2)",
                         "rgba(54, 162, 235, 0.2)",
                         "rgba(255, 206, 86, 0.2)",
                         "rgba(75, 192, 192, 0.2)",
                         "rgba(153, 102, 255, 0.2)",
-                        "rgba(255, 159, 64, 0.2)"
                     ],
                     borderColor: [
                         "rgba(255, 99, 132, 1)",
@@ -217,7 +270,6 @@ function picoInfraChart() {
                         "rgba(255, 206, 86, 1)",
                         "rgba(75, 192, 192, 1)",
                         "rgba(153, 102, 255, 1)",
-                        "rgba(255, 159, 64, 1)"
                     ],
                     borderWidth: 1
                 }
@@ -225,16 +277,26 @@ function picoInfraChart() {
         }
     });
 }
-function pontosCarteiraChart() {
+async function pontosCarteiraChart() {
+    let labels = [];
+    let data = [];
+    const bairros = localStorage.getItem('bairros').split(',');
+    const ano = localStorage.getItem('ano');
+    const pontosAplicados = await axios.post('http://localhost:3001/api/pontosAplicados', { 'ano': ano, 'bairro': bairros });
+    await pontosAplicados.data.forEach(valor => {
+        labels.push(valor.nome); data.push(parseInt(valor.media))
+    });
+
+
     var pontosCarteira = document.getElementById("pontosCarteira").getContext("2d");
     var myChart = new Chart(pontosCarteira, {
         type: "pie",
         data: {
-            labels: ["2015", "2016", "2017", "2018", "2019", "Orange"],
+            labels: labels,
             datasets: [
                 {
                     label: "# of Votes",
-                    data: [1000, 2505, 6596, 3262, 3362, 3363],
+                    data: data,
                     backgroundColor: [
                         "rgba(255, 99, 132, 0.2)",
                         "rgba(54, 162, 235, 0.2)",
